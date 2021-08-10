7dfc78b7da3fb298a1d3c99d2a849be2ae7bfce7
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2021-08-10 14:40:23 -0300

    Vue 3 is on fire!

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
+import { createApp } from 'vue';
+
+import Home from './components/Home.vue';
+
+const app = createApp({
+    components : [Home]
+});
+
+//app.component('Home', Home);
+
+app.mount('#app');
+
 require('./bootstrap');
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1> {{ greeting }}  </h1>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    setup: () => ({
+        greeting : 'Olá! Seja bem-vindo!'
+    })
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
